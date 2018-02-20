62597d2a2aacd5edf54bf290f5ec7fe87b70b8b3
Patrick <patldown@gmail.com>
2018-02-20 10:56:47 -0500

    New Additions w/ Notes

--- financial_project/settings.py
+++ financial_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio_balance.apps.PortfolioBalanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- portfolio_balance/models.py
+++ portfolio_balance/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    ### Each of the below variables represent a field based on
+    ### --- models.Model subclass. Field classes are used...
+    ### --- e.g. CharField/DateTimeField
+    question_text = models.CharField(max_length=200)
+    ### notice we provided a human readable fieldname or 'date published',
+    ### --- This is optional
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    ### Models.Foreign_Key defines a relationship as in each choice is related
+    ### --- to a single question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
