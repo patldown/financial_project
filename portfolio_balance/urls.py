d8aa51edf16e74039e0148e7f6990cbf8c9834bd
Patrick <patldown@gmail.com>
2018-02-22 10:59:31 -0500

    Started working on Views
    Added a tremendous amount of effort in understanding relationship between html and python. Worke don getting pages to link

--- portfolio_balance/urls.py
+++ portfolio_balance/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    ### Baseline path for initial request
     path('', views.index, name='index'),
+    ### notice the change when adding the next
+    path('<int:question_id>/', # This sets up index from previous item
+         views.detail, # This adds the detail function as a subfolder
+         name = 'detail'),
+    path('<int:question_id>/results/', views.results, name = 'results'),
+    path('<int:question_id>/vote/', views.vote, name = 'vote')
+    
 ]
--- portfolio_balance/views.py
+++ portfolio_balance/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Portfolio (Re)Balancing Application</h1>")
+    ### Add latest_question_list to grab last 5 questions after ordering
+    # by date
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    ### Here we add reference to template, which we do not need
+    #template = loader.get_template('portfolio_balance/index.html')
+    context = {
+            'latest_question_list': latest_question_list,
+            }
+    ### The below comment shows the HttpResponse v. the next used line
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'portfolio_balance/index.html', context)
+    # Commented out the bottom line as we are adding more
+    #return HttpResponse("<h1>Portfolio (Re)Balancing Application</h1>")
+
+def detail(request, question_id):
+    ### Error handling has been done here:
+    
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist.")
+    
+    question = get_object_or_404(Question, pk=question_id)
+    
+    return render(request, 'portfolio_balance/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "<h1>You're looking at the results of question %s.</h1>"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
